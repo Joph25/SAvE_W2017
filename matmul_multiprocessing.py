3efe307f1e29f618b8967447e65a2ef39a5bbc4f
Joph25 <philipp.jonas@gmx.at>
2017-11-19 20:59:37 +0100

    matmul_multiprocessing: Small changes

--- matmul_multiprocessing.py
+++ matmul_multiprocessing.py
@@ -3,26 +3,26 @@ import random
 import time
 import numpy
 
-MATNR=50
-ns1=1000
+MATNR=10
+ns1=2000
 nz1=ns1
 
-def mat_create():
-        # Creates a matrix with random numbers [-10000 ... 10000]
-        m1=[[float(x)/10 for x in random.sample(range(-100000,100000), ns1)] for s in range(nz1)]
-        return m1
+
+def mat_create_multi(nr):
+    # Creates nr matrices with random numbers [-10000 ... 10000]
+    m=[]
+    for i in range (nr):
+        m.append([[float(x) / 10 for x in random.sample(range(-100000, 100000), ns1)] for s in range(nz1)])
+    return m
 
 # ---------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
     m=[]
     mat_out=[]
     start_time=time.time()      #initialising timer
-    for i in range (MATNR):     #creating MATNR matrices to multiply
-        m.append(mat_create())
-        #print (m[i])
-        #print("------------------------------")
-
+    m=mat_create_multi(MATNR)
     print("Creation took ",time.time()-start_time, " seconds")
+
     start_time=time.time()      #resetting timer
     for i in range(MATNR):      #multiplying matrices one by one using numpy.dot
         mat_out.append(numpy.dot(m[i], m[(i + 1) % (len(m))]))
@@ -35,8 +35,8 @@ if __name__ == '__main__':
     for i in range (MATNR):     #creating a list of matrices offset by one
         m_offset.append(m[(i+1)%len(m)])
 
-    p = multiprocessing.Pool(processes=10)      #creating a pool for multiprocessing
-    M = p.starmap(numpy.dot, zip(m,m_offset))   #multiplying matrices using multiprocessing
+    p2 = multiprocessing.Pool(processes=10)      #creating a pool for multiprocessing
+    M = p2.starmap(numpy.dot, zip(m,m_offset))   #multiplying matrices using multiprocessing
     print("Multiprocessing took",time.time() - start_time, " seconds")
 
     """
