88b2fa02b895fee6e67a6120efeb760dd6f6f44f
Joph25 <philipp.jonas@gmx.at>
2018-01-14 21:50:05 +0100

    small changes

--- Laplace.py
+++ Laplace.py
@@ -3,7 +3,11 @@ import copy
 
 class Potential:
     potential=[]
+    starting_pot=[]
 
+    def __init__(self):
+        self.potential=[]
+        self.starting_pot=[]
 
     def get_pot(self, filename):
         tmp_potential=[]
@@ -20,7 +24,7 @@ class Potential:
                 else:
                     wert=float(tmp_potential[i][j])
                     self.potential[i][j]=[wert,False]
-
+        self.starting_pot=self.potential
 
 
     def print_potential(self):
@@ -36,7 +40,9 @@ class Potential:
 
     def calc_potential(self):
         recalc=True
+        counter=0
         while(recalc==True):
+            counter+=1
             prev_potential=copy.deepcopy(self.potential)        #saving potential bevore calculation for comparison
             self.calc_potential_step()
             recalc=False
@@ -46,12 +52,13 @@ class Potential:
                     if abs(residue)>PRECISION:
                         recalc=True
                         continue
+        return counter
 
     def print_to_file(self,filename):
         f = open(filename, 'w')
         for i in range(len(self.potential)):
             for j in range(len(self.potential[i])):
-                f.write(str(self.potential[i][j][0]))
+                f.write(str("%.2f"%self.potential[i][j][0]))
                 f.write(" ")
             f.write("\n")
         f.close()
@@ -79,7 +86,7 @@ class Potential:
         while (recalc == True):
             self.print_to_file("pot_out.dat")
             counter+=1
-            tmp_potential = copy.deepcopy(self.potential)
+            tmp_potential = copy.deepcopy(self.potential)       #copy previous potential for comnparison and residue calculation
             potential_preomega=self.calc_potential_step_tmp()
             recalc = False
             for i in range(len(self.potential)):
@@ -87,7 +94,11 @@ class Potential:
                     residue=(potential_preomega[i][j][0] - tmp_potential[i][j][0])
                     if (abs(residue) > PRECISION):
                         recalc = True
+                    if (abs(residue)> 10000):
+                        print ("residue too big @ counter " + str(counter))
+                        quit()
                     if(self.potential[i][j][1]==False):
+                        pot_now=tmp_potential[i][j][0] + omega * residue
                         self.potential[i][j][0]=tmp_potential[i][j][0]+omega*residue
         return counter
 # ---------------------------------------------------------------------------------------------------------
@@ -95,10 +106,11 @@ def main():
     dach=Potential()
     #for i in range(16,30):
     #omega=float(i)/10
-    omega=1.9
-    dach.get_pot("pl_ko60x60.dat")
-    counter=dach.calc_potential_SOR(omega)
-    print ("With omega "+str(omega)+" the calculation took "+str(counter)+" steps")
+    omega=1.5
+    dach.get_pot('laplace_daten/zyl-260x260-100-125-0.dat')
+    #counter=dach.calc_potential_SOR(omega)#
+    #print ("With omega "+str(omega)+" the calculation took "+str(counter)+" steps")
+    counter=dach.calc_potential()
     dach.print_to_file("pot_out.dat")
     dach.print_for_gnuplot("gnu_pot_out.dat")
 
